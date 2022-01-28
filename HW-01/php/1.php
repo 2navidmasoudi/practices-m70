d9969466102b63c4e0ba819873f8afdffee67283
Navid Masoudi <2navidmasoudi@gmail.com>
2022-01-28 23:19:20 +0330

    added HW01 / php / 2

--- HW-01/php/1.php
+++ HW-01/php/1.php
@@ -3,9 +3,12 @@
 $t = readline();
 
 if ($t > 100) {
+    // higher than 100
     echo "Steam";
 } else if ($t < 0) {
+    // lower than 0
     echo "Ice";
 } else {
+    // between 0 and 100 (+ 0 and 100)
     echo "Water";
 }
---
+++ HW-01/php/2.php
@@ -0,0 +1,15 @@
+<?php
+
+// input 3 numbers in array
+for ($i = 0; $i < 3; $i++) {
+    $len[] = readline();
+}
+
+// we sort it to find 2 smaller input
+sort($len);
+
+// pow is power (num , exp)
+if (pow($len[0], 2) + pow($len[1], 2) == pow($len[2], 2))
+    echo "YES";
+else
+    echo "NO";
