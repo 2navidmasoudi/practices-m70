b881a777a701d7336045e47e843339af21cb302f
Navid Masoudi <2navidmasoudi@gmail.com>
2022-01-28 23:51:29 +0330

    better code format for php / 4

--- HW-01/php/4.php
+++ HW-01/php/4.php
@@ -10,10 +10,18 @@ $n = readline();
 
 // for upper triangle
 for ($i = 1; $i <= $n; $i += 2) {
-    echo str_repeat(" ", ($n - $i) / 2) . str_repeat("*", $i) . str_repeat(" ", $n - $i) . str_repeat("*", $i) . "\n";
+    echo str_repeat(" ", ($n - $i) / 2)
+        . str_repeat("*", $i)
+        . str_repeat(" ", $n - $i)
+        . str_repeat("*", $i)
+        . "\n";
 }
 
 // for lower triangle
 for ($i = $n - 2; $i >= 1; $i -= 2) {
-    echo str_repeat(" ", ($n - $i) / 2) . str_repeat("*", $i) . str_repeat(" ", $n - $i) . str_repeat("*", $i) . "\n";
+    echo str_repeat(" ", ($n - $i) / 2)
+        . str_repeat("*", $i)
+        . str_repeat(" ", $n - $i)
+        . str_repeat("*", $i)
+        . "\n";
 }
