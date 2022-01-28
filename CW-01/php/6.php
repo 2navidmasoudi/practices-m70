e9b1e151754a640ab1cb7d174ca570f287ed78ca
Navid Masoudi <2navidmasoudi@gmail.com>
2022-01-28 22:38:07 +0330

    added HW1 pdf

--- CW-01/php/6.php
+++ CW-01/php/6.php
@@ -19,7 +19,7 @@ $len = strlen($n);
 $temp = 0;
 for ($i = 0; $i < $len; $i++) {
     $temp = $n % 10;
-    $n = $n / 10;
+    $n /= 10;
     echo "$temp: ";
     for ($j = 0; $j < $temp; $j++) {
         echo $temp;
