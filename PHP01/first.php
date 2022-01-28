a7d7510440749d1a321f67bf2582264bd26c21db
Navid Masoudi <2navidmasoudi@gmail.com>
2022-01-28 12:09:25 +0330

    variables, conditions and loops

--- PHP01/first.php
+++ PHP01/first.php
@@ -1,17 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+// echo "Hello World!";
+# This is a comment
+/*
+Multiline comment
+$lastname = "Masoudi"; */
 
-<body>
-    <?php
-    echo "<h1>Hello World!</h1>";
-    ?>
-</body>
+$a = 10;
+$b = 'Navid';
+$c = 1.0;
+$d = true;
+$e = "$b Masoudi \"";
 
-</html>
+$b = 15;
+$c = 10;
+
+if ($a == 10)
+    echo "a is 10 ";
+else
+    echo "a is not 10 ";
+
+if ($b) {
+    // for more lines...
+    echo $b;
+} else {
+    echo 'no b';
+}
+
+echo "\r \r";
+
+// 0 and "" is always false
+
+for ($i = 0; $i < 20; $i++) {
+    if ($i === 10) break;
+    echo $i . " ";
+}
+
+echo "\n";
+
+for ($i = 9; $i >= 0; $i--) {
+    echo $i . " ";
+}
+
+echo "\n";
