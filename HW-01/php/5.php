801aeba01a956f59e7a3fc44c9611a7a3cb56e8d
Navid Masoudi <2navidmasoudi@gmail.com>
2022-01-29 00:30:17 +0330

    Added HW01 / PHP / 5 wow i did it :D

---
+++ HW-01/php/5.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Text Editor using php</title>
+</head>
+
+<style>
+    p {
+        font-size: 20px;
+    }
+</style>
+
+<body style="padding-left: 10px;">
+    <form action="" method="post">
+        <p>
+            Write your text here:
+            <input type="text" name="txt">
+        </p>
+        <p>
+            Fontsize here:
+            <input type="number" name="size">
+            px
+        </p>
+        <p>
+            and the color:
+            <input type="color" name="clr">
+        </p>
+
+        <input type="submit" value="Show Me!">
+    </form>
+
+    <?php
+
+    // We get color, text and size by post method in form and output in php :)
+    $color ??= $_POST['clr'];
+    $text ??= $_POST['txt'];
+    $size ??= $_POST['size'] . 'px';
+
+    echo "<p style='font-size: $size;color: $color'>$text<p/>";
+
+    ?>
+
+</body>
+
+</html>
