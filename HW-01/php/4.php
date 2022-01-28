cf5e70f3fc9c566e9aec971b208e925bc18cc5bb
Navid Masoudi <2navidmasoudi@gmail.com>
2022-01-28 23:49:29 +0330

    added HW01 / PHP / 4

---
+++ HW-01/php/4.php
@@ -0,0 +1,19 @@
+<?php
+
+# https://quera.org/problemset/9773/
+
+$n = readline();
+// n % 2 is always 1
+
+// str_repeat repeats an string x times
+// str_repeat ($string, $times)
+
+// for upper triangle
+for ($i = 1; $i <= $n; $i += 2) {
+    echo str_repeat(" ", ($n - $i) / 2) . str_repeat("*", $i) . str_repeat(" ", $n - $i) . str_repeat("*", $i) . "\n";
+}
+
+// for lower triangle
+for ($i = $n - 2; $i >= 1; $i -= 2) {
+    echo str_repeat(" ", ($n - $i) / 2) . str_repeat("*", $i) . str_repeat(" ", $n - $i) . str_repeat("*", $i) . "\n";
+}
