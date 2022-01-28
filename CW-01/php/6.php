d583ee721830310799a11adc0251ed6b537cb04c
Navid Masoudi <2navidmasoudi@gmail.com>
2022-01-28 17:14:45 +0330

    php cw-01 question 6

--- CW-01/php/6.php
+++ CW-01/php/6.php
@@ -2,27 +2,27 @@
 
 # https://quera.org/problemset/9774/
 
-$input = readline();
-$number = str_split($input);
-foreach ($number as $n) {
-    echo "$n: ";
-    for ($i = 0; $i < $n; $i++) {
-        echo $n;
-    }
-    echo "\n";
-}
+// $input = readline();
+// $number = str_split($input);
+// foreach ($number as $n) {
+//     echo "$n: ";
+//     for ($i = 0; $i < $n; $i++) {
+//         echo $n;
+//     }
+//     echo "\n";
+// }
 
 // or
 
-// $n = strrev(readline());
-// $t = 0;
-// while ($n) {
-//     $t = floor($n) % 10;
-//     echo "$t: ";
-//     for ($i = 0; $i < $t; $i++) {
-//         echo $t;
-//     }
-//     echo "\n";
-//     $n = floor($n) / 10;
-//     if ($n == 0) break;
-// }
+$n = (int) strrev(readline());
+$len = strlen($n);
+$temp = 0;
+for ($i = 0; $i < $len; $i++) {
+    $temp = $n % 10;
+    $n = $n / 10;
+    echo "$temp: ";
+    for ($j = 0; $j < $temp; $j++) {
+        echo $temp;
+    }
+    echo "\n";
+}
