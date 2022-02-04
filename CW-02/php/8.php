ecedc4fcefc0ca06404f84da080ff7622d84c5a3
Navid Masoudi <2navidmasoudi@gmail.com>
2022-02-04 15:29:53 +0330

    adde 8

--- CW-02/php/8.php
+++ CW-02/php/8.php
@@ -7,6 +7,19 @@ Expected Output : The shortest array length is 1. The longest array length is 4.
 */
 
 $arr = array("abcd", "abc", "de", "hjjj", "g", "wer");
-
 $min = 1000;
 $max = 0;
+foreach ($arr as $str) {
+    if (strlen($str) < $min) {
+        $min = strlen($str);
+    }
+    if (strlen($str) > $max) {
+        $max = strlen($str);
+    }
+}
+echo "The shortest array length is $min. The longest array length is $max.";
+// or
+$lengths = array_map('strlen', $data);
+$min = min($lengths);
+$max = max($lengths);
+echo "The shortest array length is $min. The longest array length is $max.";
