75a13780177e07e39e052bada9e05dcb43008229
Navid Masoudi <2navidmasoudi@gmail.com>
2022-02-04 15:16:29 +0330

    CW02 / php

---
+++ CW-02/php/8.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Write a PHP script to get the shortest/longest string length from an array. 
+Sample arrays : ("abcd","abc","de","hjjj","g","wer")
+Expected Output : The shortest array length is 1. The longest array length is 4.
+*/
+
+$arr = array("abcd", "abc", "de", "hjjj", "g", "wer");
+
+$min = 1000;
+$max = 0;
