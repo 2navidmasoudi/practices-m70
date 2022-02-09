9e7ed8b3494285bfe4baa30a0b50a1954676e663
Navid Masoudi <2navidmasoudi@gmail.com>
2022-02-09 08:55:46 +0330

    HW-02 / 2 font founded from sanjesh.org and added

--- HW-02/2.php
+++ HW-02/2.php
@@ -6,10 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/2.css">
 </head>
 
-<body>
-    <div></div>
+<body dir="rtl">
+    <div>
+        <label for="firsname">نام<span>*</span></label>
+    </div>
 </body>
 
 </html>
