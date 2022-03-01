257aa546fcedd0fc790209e0f415a89126b19bb2
Navid Masoudi <2navidmasoudi@gmail.com>
2022-03-01 18:55:57 +0330

    for exam

--- PHP-05/test.php
+++ PHP-05/test.php
@@ -1,1 +1,10 @@
 <?php
+
+$obj_1 = array("one" => "One");
+$obj_2 = array("one" => "One");
+
+for ($i = 1; $i < 3; $i++) {
+    foreach (${"obj_$i"} as $key => $value) {
+        echo $key . $value . "\n";
+    }
+}
