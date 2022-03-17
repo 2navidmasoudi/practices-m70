39147c8b1bf49c939188d1906ef9228883d72f0f
Navid Masoudi <2navidmasoudi@gmail.com>
2022-03-17 15:50:07 +0330

    updated

--- CW-08/jQuery/tel.php
+++ CW-08/jQuery/tel.php
@@ -1,3 +1,17 @@
 <?php
 
-var_dump($_POST);
+if (isset($_POST['submit'])) {
+    extract($_POST);
+    $file = file_get_contents('contacts.json');
+    $contacts = json_decode($file, true);
+    $contacts[] = [
+        "fname" => $fname,
+        "lname" => $lname,
+        "tel" => $tel,
+        "gender" => $gender,
+        "detail" => $detail,
+    ];
+    var_dump($contacts);
+    $data = json_encode($contacts, JSON_PRETTY_PRINT);
+    file_put_contents('contacts.json', $data);
+}
