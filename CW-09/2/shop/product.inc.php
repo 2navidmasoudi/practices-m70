a20c2ee177cb8dcabf81b69c525080e83368764f
Navid Masoudi <2navidmasoudi@gmail.com>
2022-03-31 23:00:35 +0430

    product done

--- CW-09/2/shop/product.inc.php
+++ CW-09/2/shop/product.inc.php
@@ -8,17 +8,95 @@ class Product
 
     public function __construct(string $name, int $price, array $options)
     {
-        // TODO
+        $this->name = $name;
+        $this->price = $price;
+        $this->options = $options;
     }
-
-    // TODO
+    
+	/**
+	 * 
+	 * @return string
+	 */
+	function getName(): string {
+		return $this->name;
+	}
+	
+	/**
+	 * 
+	 * @param string $name 
+	 * @return Product
+	 */
+	function setName(string $name): self {
+		$this->name = $name;
+		return $this;
+	}
+	/**
+	 * 
+	 * @return int
+	 */
+	function getPrice(): int {
+		return $this->price;
+	}
+	
+	/**
+	 * 
+	 * @param int $price 
+	 * @return Product
+	 */
+	function setPrice(int $price): self {
+		$this->price = $price;
+		return $this;
+	}
+	/**
+	 * 
+	 * @return array
+	 */
+	function getOptions(): array {
+		return $this->options;
+	}
+	
+	/**
+	 * 
+	 * @param array $options 
+	 * @return Product
+	 */
+	function setOptions(array $options): self {
+		$this->options = $options;
+		return $this;
+	}
 }
 
 class Shirt extends Product
 {
     private string $size;
 
-    // TODO
+	/**
+	 * 
+	 * @return string
+	 */
+	function getSize(): string {
+		return $this->size;
+	}
+	
+	/**
+	 * 
+	 * @param string $size 
+	 * @return Shirt
+	 */
+	function setSize(string $size): self {
+        switch ($size) {
+            case "xm":
+            case "md":
+            case "lg":
+            case "xlg":
+            case "2xlg":
+                $this->size = $size;
+                break;
+            default:
+                throw new Exception('Not good size.');
+        }
+		return $this;
+	}
 }
 
 class Pants extends Product
@@ -26,4 +104,31 @@ class Pants extends Product
     private int $size;
 
     // TODO
+	/**
+	 * 
+	 * @return int
+	 */
+	function getSize(): int {
+		return $this->size;
+	}
+	
+	/**
+	 * 
+	 * @param int $size 
+	 * @return Pants
+	 */
+	function setSize(int $size): self {
+        if ($size % 2 == 0 and $size >= 30 and $size <= 60)
+		    $this->size = $size;
+        else 
+            throw new Exception('Not good size.');
+        return $this;
+	}
 }
+
+// $pant = new Pants('a', 2, ['color' => 'red']);
+// $pant->setSize(30);
+
+// $shirt = new Shirt('a', 3, ['color' => 'blue']);
+// $shirt->setSize('xm');
+// print_r($shirt);
